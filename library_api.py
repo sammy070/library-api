df604567f0f12bd72211ca58eb8839c35cde01eb
sammy070 <ashaald35@gmail.com>
2025-04-29 14:24:45 +0530

    Create library_api.py

---
+++ library_api.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Example in-memory book data
+books = [
+    {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
+    {"id": 2, "title": "A Brief History of Time", "author": "Stephen Hawking"}
+]
+
+@app.route('/api/books', methods=['GET'])
+def get_books():
+    return jsonify(books)
+
+@app.route('/api/books', methods=['POST'])
+def add_book():
+    data = request.json
+    new_book = {
+        "id": len(books) + 1,
+        "title": data.get("title"),
+        "author": data.get("author"),
+        "category": data.get("category", "General")
+    }
+    books.append(new_book)
+    return jsonify(new_book), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
